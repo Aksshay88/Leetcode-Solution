c9f5212565c8d2688621cb015b030a6bbd960e67
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2023-09-12 21:22:44 +0530

    Time: 1951 ms (65.38%), Space: 0B (100.00%) - LeetHub

---
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -0,0 +1,7 @@
+SELECT product_id, year as first_year, quantity, price 
+from Sales 
+where (product_id, year) in (
+        SELECT product_id, min(year) as year 
+        from Sales 
+        group by product_id 
+        );
