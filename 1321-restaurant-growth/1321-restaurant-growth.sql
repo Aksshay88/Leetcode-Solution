cd04aa077996b7e1b299df2bcd305d5b8ca5e612
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2023-10-02 15:10:20 +0530

    Time: 730 ms (80.12%), Space: 0B (100.00%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,8 @@
+SELECT visited_on, amount, ROUND(amount/7, 2) average_amount
+FROM (
+    SELECT DISTINCT visited_on, 
+    SUM(amount) OVER(ORDER BY visited_on RANGE BETWEEN INTERVAL 6 DAY   PRECEDING AND CURRENT ROW) amount, 
+    MIN(visited_on) OVER() 1st_date 
+    FROM Customer
+) t
+WHERE visited_on>= 1st_date+6;
