8d7dd1deed88dd5ce38682a6bbc96257585e1ead
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2023-10-02 15:06:47 +0530

    Time: 590 ms (66.34%), Space: 0B (100.00%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,10 @@
+SELECT frnd_id as id, count(*) as num 
+FROM
+(SELECT requester_id as frnd_id
+FROM RequestAccepted 
+UNION ALL
+SELECT accepter_id as frnd_id
+FROM RequestAccepted) as t
+GROUP BY frnd_id
+ORDER BY num DESC
+limit 1
