aad73e420dc27158409a32948a3d24804f096669
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2023-11-16 18:47:26 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (12.66%) - LeetHub

---
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.java
@@ -0,0 +1,32 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * int guess(int num);
+ */
+
+public class Solution extends GuessGame {
+    public int guessNumber(int n) {
+        int s = 0;
+        int e = n;
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            int x = guess(mid);
+            if(x==0){
+                return mid;
+            }
+            else if(x==-1){
+                e = mid-1;
+            }
+            else{
+                s = mid+1;
+            }
+
+        }
+        return -1;
+        
+        
+    }
+}
