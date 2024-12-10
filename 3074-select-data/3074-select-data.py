d1ed27872dc3b6990d2699fcfcf56a23979da6d5
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2024-12-10 13:34:26 +0530

    Time: 269 ms (98.59%), Space: 66.4 MB (95.27%) - LeetHub

---
+++ 3074-select-data/3074-select-data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students["student_id"] == 101, ["name", "age"]]
+    return students.loc[students["student_id"] == 101, "name" :]
+    return students[students['student_id'] == 101][['name', 'age']]
