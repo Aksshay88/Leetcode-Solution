4bb4d90618b252db698b6b7269041cbae2cd4c71
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2024-12-10 10:53:24 +0530

    Time: 55 ms (77.45%), Space: 49.4 MB (57.89%) - LeetHub

---
+++ 2743-debounce/2743-debounce.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Function} fn
+ * @param {number} t milliseconds
+ * @return {Function}
+ */
+var debounce = function(fn, t) {
+    let timeout = null;
+    
+    return function(...args) {
+        if(timeout)
+            clearTimeout(timeout);
+        timeout = setTimeout(() => fn(...args), t);
+    }
+};
+
