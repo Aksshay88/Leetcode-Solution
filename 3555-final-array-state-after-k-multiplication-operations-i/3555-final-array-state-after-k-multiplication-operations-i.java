5a90bd7fb115f2de8af2221069e0cf13e18c407d
Aksshay88 <119944779+Aksshay88@users.noreply.github.com>
2024-12-16 11:30:16 +0530

    Time: 1 ms (100%), Space: 44.9 MB (50.55%) - LeetHub

---
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        for(int i=0;i<k;i++){
+            int m=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[j]<nums[m]){
+                    m=j;
+                }
+            }
+            nums[m]*=multiplier;
+        }
+        return nums;
+    }
+}
